b2541fd518877b16245b6e7e88455cfe03b52c23
Hsien Heng Lie <hsien@bbd.co.za>
2023-02-22 19:30:42 +0200

    created sp_transactionInsert

---
+++ UberForFish/Stored Procedures/transactionInsert.sql
@@ -0,0 +1,30 @@
+﻿--Insert transaction when whale confirms fish trip
+
+CREATE PROCEDURE [dbo].[sp_transactionInsert]
+		@whaleId INT,
+		@fishId INT,
+		@pickUpLocation GEOGRAPHY,
+		@dropOffLocation GEOGRAPHY
+AS
+	BEGIN TRY
+		BEGIN TRAN
+			INSERT INTO [dbo].[Transaction]
+			   ([WhaleId],
+			   [DateTime],
+			   [FishId],
+			   [PickUpLocation],
+			   [DropOffLocation]
+			    )
+			 VALUES
+				(@whaleId,
+				GETDATE(),
+				@fishId,
+				@pickUpLocation,
+				@dropOffLocation
+				)
+		COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK;
+		THROW 
+	END CATCH
