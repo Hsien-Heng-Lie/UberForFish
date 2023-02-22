f110970ef88ec6f55d4fa2b64044124fd9937d62
Hsien Heng Lie <hsien@bbd.co.za>
2023-02-22 19:47:37 +0200

    added [calculateWhaleTravelCost] function

---
+++ UberForFish/UDF/calculateWhaleTravelCost.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION [dbo].[calculateWhaleTravelCost]
+(
+	@whaleId int,
+	@distance int
+)
+RETURNS FLOAT
+AS
+BEGIN
+	DECLARE @cost FLOAT
+
+		SET @cost = (
+					SELECT  wt.CostPerMile * [dbo].[convertMeterToNauticalMile](@distance) as cost
+							from [dbo].[Whale] as w
+							inner join [dbo].[WhaleType] wt
+							on w.WhaleTypeId = wt.WhaleTypeId
+							where w.WhaleId = @whaleId
+					)
+
+	RETURN @cost
+END
