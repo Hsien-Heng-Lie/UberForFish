e293cade159bfbf9afbf63894e24119188b9fd6d
Hsien Heng Lie <hsien@bbd.co.za>
2023-02-23 10:34:09 +0200

    added sp_ratingUpSert to update and insert ratings

---
+++ UberForFish/Stored Procedures/sp_ratingUpSert.sql
@@ -0,0 +1,58 @@
+﻿--Stored Procedure to update or insert a rating
+CREATE PROCEDURE [dbo].[sp_ratingUpSert]
+	@tranId int ,
+	@usertype INT,
+	@rating INT
+AS
+	BEGIN TRY
+		BEGIN TRAN
+			DECLARE @whaleId INT,
+					@fishId INT,
+					@whaleRating INT = NULL,
+					@fishRating INT = NULL
+
+			SELECT @fishId = FishId FROM [dbo].[Transaction] WHERE [TransactionId] = @tranId
+			SELECT @whaleId = WhaleId FROM [dbo].[Transaction] WHERE [TransactionId] = @tranId
+
+			-- 1 : Whale
+			IF (@usertype = 1)
+				BEGIN
+					SET @whaleRating = @rating
+				END
+			-- 2 : Fish
+			ELSE IF (@usertype = 2)
+				BEGIN
+					SET @fishRating = @rating
+				END
+			
+			MERGE [dbo].[Rating]
+			USING 
+			(
+			SELECT 
+				@whaleRating as [WhaleRating],
+				@fishRating as [FishRating],
+				@tranId as [TransactionId]
+			) AS RatingUpSert
+			ON [Rating].[TransactionId] = [RatingUpSert].[TransactionId]
+			WHEN NOT MATCHED THEN
+			INSERT(
+				[TransactionId],
+				[WhaleRating],
+				[FishRating]
+			)
+			VALUES(
+				@tranId,
+				@whaleRating,
+				@fishRating
+			)
+			WHEN MATCHED THEN
+			UPDATE SET  
+					[WhaleRating] = COALESCE([Rating].[WhaleRating], @whaleRating),
+					[FishRating] = COALESCE([Rating].[FishRating], @fishRating)
+					;
+		COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK;
+		THROW 
+	END CATCH
--- UberForFish/Tables/Rating.sql
+++ UberForFish/Tables/Rating.sql
@@ -1,13 +1,11 @@
 ﻿CREATE TABLE [dbo].[Rating]
 (
 	[RatingId] INT NOT NULL IDENTITY(1,1), 
-    [WhaleId] INT NULL, 
-    [FishId] INT NULL, 
     [TransactionId] INT NULL, 
     [WhaleRating] INT NULL, 
     [FishRating] INT NULL, 
     CONSTRAINT [PK_RatingId] PRIMARY KEY ([RatingId]), 
-    CONSTRAINT [FK_Rating_WhaleId] FOREIGN KEY ([WhaleId]) REFERENCES [dbo].[Whale]([WhaleId]),
-    CONSTRAINT [FK_Rating_FishId] FOREIGN KEY ([FishId]) REFERENCES [dbo].[Fish]([FishId]),
-    CONSTRAINT [FK_Rating_TransactionId] FOREIGN KEY ([TransactionId]) REFERENCES [dbo].[Transaction]([TransactionId])
+    CONSTRAINT [FK_Rating_TransactionId] FOREIGN KEY ([TransactionId]) REFERENCES [dbo].[Transaction]([TransactionId]), 
+    CONSTRAINT [CK_Rating_WhaleRating] CHECK ([WhaleRating] < 6),
+    CONSTRAINT [CK_Rating_FishRating] CHECK ([FishRating] < 6)
 )
